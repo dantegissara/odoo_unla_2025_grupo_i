da93858e5b3ed66fc525dc20ac3da60dea92fff5
Leandro <leanroman99@gmail.com>
2025-08-28 15:18:15 -0300

    se agergo modulo real_estate con grupos y vista de busqueda, resuelto hasta ejercicio 17

--- unla-desarrollos/real_estate/__manifest__.py
+++ unla-desarrollos/real_estate/__manifest__.py
@@ -7,6 +7,7 @@
     "license": "LGPL-3",
     "depends": ["base"],
     "data": [
+        "security/real_estate_res_groups.xml",
         "security/ir.model.access.csv",
     	"views/estate_property_views.xml",
         "views/real_estate_menuitem.xml"

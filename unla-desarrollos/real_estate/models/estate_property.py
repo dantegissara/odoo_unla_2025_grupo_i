fdcda524af178e91f0b6a8de6b3b1824c02b3973
LeandroMussi99 <leanroman99@gmail.com>
2025-09-10 22:29:41 -0300

    Avance hasta ejercicio 22 del modulo real_estate: campo state y vista de lista custom en estate.property

--- unla-desarrollos/real_estate/models/estate_property.py
+++ unla-desarrollos/real_estate/models/estate_property.py
@@ -1,5 +1,6 @@
 from odoo import fields, models, api
 from dateutil.relativedelta import relativedelta
+from datetime import date, timedelta
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
@@ -8,9 +9,9 @@ class EstateProperty(models.Model):
     name = fields.Char(string="Título", required=True)
     description = fields.Text(string="Descripción")
     postcode = fields.Char(string="Código Postal")
-    date_availability = fields.Date(string="Fecha disponibilidad")
+    date_availability = fields.Date(string="Fecha disponibilidad",copy=False, default=lambda self: date.today() + timedelta(days=90))
     expected_price = fields.Float(string="Precio esperado")
-    selling_price = fields.Float(string="Precio de venta")
+    selling_price = fields.Float(string="Precio de venta",copy=False)
     bedrooms = fields.Integer(string="Habitaciones", default=2)
     living_area = fields.Integer(string="Superficie cubierta")
     facades = fields.Integer(string="Fachadas")
@@ -27,3 +28,16 @@ class EstateProperty(models.Model):
         string="Orientación del jardín",
     )
     garden_area = fields.Integer(string="Superficie jardín")
+    state = fields.Selection(
+        [
+            ("nuevo", "Nuevo"),
+            ("oferta_recibida", "Oferta Recibida"),
+            ("oferta_aceptada", "Oferta Aceptada"),
+            ("vendido", "Vendido"),
+            ("cancelado", "Cancelado"),
+        ],
+        string="Estado",
+        required=True,
+        default="nuevo",
+        copy=False
+    )

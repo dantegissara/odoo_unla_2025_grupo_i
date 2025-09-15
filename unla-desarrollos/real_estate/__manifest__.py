37ab7b9c3d142117b4e145ef2f6735d362c4f8ff
LeandroMussi99 <leanroman99@gmail.com>
2025-09-14 21:49:57 -0300

    Avance hasta ejercicio 28 del módulo real_estate

--- unla-desarrollos/real_estate/__manifest__.py
+++ unla-desarrollos/real_estate/__manifest__.py
@@ -10,6 +10,7 @@
         "security/real_estate_res_groups.xml",
         "security/ir.model.access.csv",
     	"views/estate_property_views.xml",
+    	"views/estate_property_type_views.xml",
         "views/real_estate_menuitem.xml"
     ],
     "installable": True,

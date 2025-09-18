c6bed1b91a199af7efbf9f7a08c3f44fca6a781d
LeandroMussi99 <leanroman99@gmail.com>
2025-09-18 14:59:53 -0300

    Ejercicio del 37 al 41 - Unidad 1 modulo real_estate completa

--- unla-desarrollos/real_estate/models/estate_property.py
+++ unla-desarrollos/real_estate/models/estate_property.py
@@ -67,3 +67,10 @@ class EstateProperty(models.Model):
         comodel_name="estate.property.tag",
         string="Etiquetas",
     )
+
+    offer_ids = fields.One2many(
+    comodel_name="estate.property.offer",
+    inverse_name="property_id",
+    string="Ofertas",
+    )
+
---
+++ unla-desarrollos/real_estate/models/estate_property_offer.py
@@ -0,0 +1,25 @@
+from odoo import models, fields
+
+class EstatePropertyOffer(models.Model):
+    _name = "estate.property.offer"
+    _description = "Oferta sobre propiedad"
+
+    price = fields.Float(string="Precio", required=True)
+    status = fields.Selection(
+        selection=[
+            ("accepted", "Aceptada"),
+            ("refused", "Rechazada"),
+        ],
+        string="Estado",
+    )
+    partner_id = fields.Many2one(
+        "res.partner",
+        string="Ofertante",
+        required=True,
+    )
+    property_id = fields.Many2one(
+        "estate.property",
+        string="Propiedad",
+        required=True,
+        ondelete="cascade",
+    )
